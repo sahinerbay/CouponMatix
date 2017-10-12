a8896f82bc6d1198921adac8c5ffddb806c78172
sahinerbay <sahin@sahinerbay.com>
2017-10-12 15:24:32 +0300

    Clean navbar.js file

--- src/script/components/_navbar.js
+++ src/script/components/_navbar.js
@@ -1,35 +1,85 @@
-let header = document.querySelector('.header');
-
-let fragment = document.createDocumentFragment();
-
-let div = document.createElement('div');
-div.className = "navbar row ";
+import dom from '../utils/_dom';
 
 
-div.innerHTML = `
-                        <div class="navbar__logo row__xs-0 row__s-0 row__md-3 row__lg-2">
-                            <a href="/"><img src="./images/logo.jpg" alt=""></a>
-                        </div>
-                        <div class="navbar__menu row row__xs-12 row__s-12 row__md-9 row__lg-10 row--gutter row--align-items">
-                            <div class="navbar__menu__links">
-                                <div class="row__xs-12 row__s-12 row__m-12 row__lg-12">
-                                    <a href="#coupons">Coupons</a>
-                                    <a  href="#samples">Samples</a>
-                                    <a  href="#giveaways">Giveaways</a>
-                                    <a href="/about.html">About</a>
-                                    <a href="javascript:void(0);" class="navbar__menu__links__hamburgerIcon" >&#9776;</a>
-                                </div>
-                            </div>
-                        </div>
-                `;
+////////////////////////////
+// CREATE HEADER ELEMENT //
+let header = dom.createElementWithClassName('div', 'header');
+// APPEND HEADER ELEMENT INTO DOM //
+dom.append(document.body, header);
 
-fragment.appendChild(div);
-header.appendChild(fragment);
+
+///////////////////////////
+// CREATE NAVBAR ELEMENT //
+let navbar = dom.createElementWithClassName('div', 'navbar row');
+// APPEND NAVBAR INTO HEADER //
+dom.append(header, navbar);
+
+
+//////////////////////////////////
+// CREATE NAVBAR--LOGO ELEMENT //
+let navbarLogo = dom.createElementWithClassName('div', 'navbar__logo row__xs-0 row__s-0 row__md-3 row__lg-2'),
+    navbarLogoLink = dom.createElementWithClassName('a', 'narbar__logo__link'),
+    navbarLogoLinkImage = dom.createElementWithClassName('img', 'narbar__logo__link__image');
+
+// SET NAVBAR--LOGO--IMAGE SRC //
+navbarLogoLinkImage.src = "./images/logo.jpg";
+
+// APPEND NAVBAR LOGO INTO NAVBAR //
+dom.append(navbar, navbarLogo);
+dom.append(navbarLogo, navbarLogoLink);
+dom.append(navbarLogoLink, navbarLogoLinkImage);
+
+
+//////////////////////////////////
+// CREATE NAVBAR--MENU ELEMENT //
+let navbarMenu = dom.createElementWithClassName('div', 'navbar__menu row row__xs-12 row__s-12 row__md-9 row__lg-10 row--gutter row--align-items');
+let navbarMenuLinks = dom.createElementWithClassName('div', 'navbar__menu__links');
+let navbarMenuLinksRow = dom.createElementWithClassName('div', 'row__xs-12 row__s-12 row__m-12 row__lg-12');
+
+// APPEND NAVBAR--MENU INTO NAVBAR //
+dom.append(navbar, navbarMenu);
+dom.append(navbarMenu, navbarMenuLinks);
+dom.append(navbarMenuLinks, navbarMenuLinksRow);
+
+// ATTRIBUTES FOR MENU LINKS //
+let linksArr = [{
+        'href': '#coupons',
+        'text': 'coupons'
+    },
+    {
+        'href': '#samples',
+        'text': 'samples'
+    },
+    {
+        'href': '#giveaways',
+        'text': 'giveaways'
+    },
+    {
+        'href': '#about',
+        'text': 'about'
+    }
+
+];
+
+// CREATE NAVBAR--MENU--LINKS //
+// INSERT INTO NAVBAR--MENU //
+for (let attributes in linksArr) {
+    let navbarMenuLinksRowA = dom.createElement('a');
+    navbarMenuLinksRowA.setAttribute('href', linksArr[attributes].href);
+    navbarMenuLinksRowA.textContent = linksArr[attributes].text;
+    dom.append(navbarMenuLinksRow, navbarMenuLinksRowA);
+}
+
+// CREATE HAMBURGER ICON //
+let navbarMenuLinksRowHamburger = dom.createElementWithClassName('a', 'navbar__menu__links__hamburgerIcon');
+navbarMenuLinksRowHamburger.innerHTML = "&#9776;";
+dom.append(navbarMenuLinksRow, navbarMenuLinksRowHamburger)
+
 
 let hamburgerIcon = document.querySelector('.navbar__menu__links__hamburgerIcon'),
     menu = document.querySelector(".navbar__menu__links");
 
-hamburgerIcon.addEventListener('click', ()=> {
+hamburgerIcon.addEventListener('click', () => {
     console.log(menu.className)
     if (menu.className === "navbar__menu__links") {
         menu.className += " responsive";
